bf3cdcb99347ada282bbf0b7104835f0b79c024b
kadir4554 <kadiregriboyun98@gmail.com>
2022-04-20 13:15:39 +0300

    Add project files.

---
+++ Dictionary/MyDictionary.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary
+{
+    class MyDictionary<T>
+    {
+
+
+        T[] _array;
+        T[] _tempArray;
+        public MyDictionary()
+        {
+                _array = new T[0];
+        }
+        public void Add(T item)
+        {
+            _tempArray = _array;
+            _array=new T[_array.Length+1];
+            for (int i = 0; i < _tempArray.Length; i++)
+            {
+                _array[i] = _tempArray[i];
+            }
+            _array[_array.Length-1] = item;
+        }        
+        public int Count
+        {
+           get{ return _array.Length; }
+        }
+        public void List()
+        {
+            for (int i = 0; i < _array.Length; i++)
+            {
+                Console.WriteLine(_array[i]);
+            }
+        }
+    }
+}
---
+++ Dictionary/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string> dictionary = new MyDictionary<string>();
+
+            dictionary.Add("Türkiye");
+            dictionary.Add("Azerbaycan");
+            dictionary.Add("Yunanistan");
+            dictionary.List();
+            Console.WriteLine(dictionary.Count);
+        }
+    }
+}
